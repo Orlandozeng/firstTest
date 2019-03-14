a7a3066cdb289682e22ba2a3e3bd04db608ae26b
zengpeng <zengpeng@yiqiandai.com>
2019-03-14 10:25:53 +0800

    初始化本地测试仓库

---
+++ testProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProduct
+//
+//  Created by zengpeng on 2019/3/14.
+//  Copyright © 2019 zengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
